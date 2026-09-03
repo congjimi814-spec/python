f79bca0900e6cdb00b761ef3c70394c40bf5b322
conglimi814 <congjimi814@gmail.com>
2026-09-03 16:44:31 +0800

    type

---
+++ study/day01/005.py
@@ -0,0 +1,26 @@
+# 求字符串长度
+a = "hello world!"
+print(len(a))
+
+# 通过索引获取字符 （索引从0开始）
+print(a[0])  # h
+print(a[11])  # !
+print(a[len(a)-1])  # !
+
+# 布尔类型（大写开头）
+b = True
+c = False
+print(b)  # True
+print(c)  # False   
+
+# 空值none
+n = None
+print(n)  # None
+
+# type函数
+print(type(1))  # <class 'int'>
+print(type(1.0))  # <class 'float'>
+print(type(a))  # <class 'str'>
+print(type(b))  # <class 'bool'>
+print(type(c))  # <class 'bool'>
+print(type(n))  # <class 'NoneType'>   
