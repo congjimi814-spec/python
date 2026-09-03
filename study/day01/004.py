e0e2ec85d83d49111468cd725c8c4d19f6cca2a7
conglimi814 <congjimi814@gmail.com>
2026-09-03 16:24:08 +0800

    calculate

---
+++ study/day01/004.py
@@ -0,0 +1,11 @@
+import math
+print(math.sqrt(4))
+print(math.sin(1))
+
+a=-1
+b=-2
+c=3
+x1 = (-b + (b** 2 -4*a*c)**0.5 )/ (2*a)
+x2 = (-b - (b** 2 -4*a*c)**0.5 )/ (2*a)
+print("x1=",x1)
+print("x2=",x2)
