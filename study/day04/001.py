edfaa336d7c46c0a9f47ec7ca0068d40fc44cf99
congjimi814-spec <congjimi814@gmail.com>
2026-09-21 17:00:23 +0800

    def 定义函数

--- study/day04/001.py
+++ study/day04/001.py
@@ -9,3 +9,7 @@ radius_0 = int(input("请输入扇形的半径："))
 
 
 print("扇形的面积为：", calculate_sector(angle_0, radius_0))
+
+print(calculate_sector(90, 10))  # 计算角度为90，半径为10的扇形面积
+print(calculate_sector(180, 5))  # 计算角度为180，半径为5的扇形面积
+print(calculate_sector(270, 8))  # 计算角度为270，半径为8的扇形面积
