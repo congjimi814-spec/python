6fdabeaba81b678d85a44faaac25a6b633153c36
congjimi814-spec <congjimi814@gmail.com>
2026-09-21 16:50:22 +0800

    def 定义函数

---
+++ study/day04/001.py
@@ -0,0 +1,11 @@
+# 函数
+# 计算扇形的面积
+def calculate_sector(angle, radius):
+    area = (angle / 360) * 3.14 * radius ** 2
+    return area 
+
+angle_0 = int(input("请输入扇形的角度："))
+radius_0 = int(input("请输入扇形的半径："))
+
+
+print("扇形的面积为：", calculate_sector(angle_0, radius_0))
